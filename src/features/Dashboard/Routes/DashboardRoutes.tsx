145b3f17251f2efb01c56aed7675b5a95cb50182
Khanh Nguyen <encacap@gmail.com>
2022-11-22 21:56:52 +0700

    feat(BLRD-5): implement i18next and react-router-dom

---
+++ src/features/Dashboard/Routes/DashboardRoutes.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
+
+import Dashboard from '../Dashboard';
+
+const DashboardRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+    </Routes>
+  );
+};
+
+export default DashboardRoutes;
