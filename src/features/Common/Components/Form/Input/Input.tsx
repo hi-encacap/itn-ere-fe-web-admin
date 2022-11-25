552d2944f27f14a5c1dc412f4fa2928a87579765
Khanh Nguyen <encacap@gmail.com>
2022-11-25 22:30:32 +0700

    fix: resolve deepsource issues

--- src/features/Common/Components/Form/Input/Input.tsx
+++ src/features/Common/Components/Form/Input/Input.tsx
@@ -8,28 +8,28 @@ export interface InputProps extends UncontrolledInputProps {
 }
 
 const Input = ({ control, name, ...inputProps }: InputProps) => {
-  if (control) {
-    const {
-      field: { onChange, onBlur, value = '' },
-      formState: { errors },
-    } = useController({
-      name,
-      control,
-    });
-
-    return (
-      <UncontrolledInput
-        name={name}
-        onChange={onChange}
-        onBlur={onBlur}
-        error={errors[name]?.message as string}
-        {...(!(value === undefined || value === null) && { value })}
-        {...omit(inputProps, ['value', 'onChange', 'onBlur'])}
-      />
-    );
+  if (!control) {
+    return <UncontrolledInput name={name} {...inputProps} />;
   }
 
-  return <UncontrolledInput name={name} {...inputProps} />;
+  const {
+    field: { onChange, onBlur, value = '' },
+    formState: { errors },
+  } = useController({
+    name,
+    control,
+  });
+
+  return (
+    <UncontrolledInput
+      name={name}
+      onChange={onChange}
+      onBlur={onBlur}
+      error={errors[name]?.message as string}
+      {...(!(value === undefined || value === null) && { value })}
+      {...omit(inputProps, ['value', 'onChange', 'onBlur'])}
+    />
+  );
 };
 
 export default Input;
