0602501eb28e39d4b263b39e2862466b6bbb6213
Khanh Nguyen <encacap@gmail.com>
2022-11-28 11:54:58 +0700

    fix: update form inputs and input errors

--- src/features/Common/Components/Form/FormElementError.tsx
+++ src/features/Common/Components/Form/FormElementError.tsx
@@ -10,17 +10,15 @@ const FormElementError = ({ error: errorProp }: FormElementErrorProps) => {
   return (
     <AnimatePresence>
       <motion.div className="relative mt-1.5 -mb-2 overflow-hidden text-sm">
-        {[errorProp].map((error) => (
-          <motion.div
-            key={slugify(error ?? '')}
-            initial={{ y: 12 }}
-            animate={{ y: 0 }}
-            exit={{ y: -12 }}
-            className="text-red-500"
-          >
-            {error}
-          </motion.div>
-        ))}
+        <motion.div
+          key={slugify(errorProp ?? '')}
+          initial={{ y: 12 }}
+          animate={{ y: 0 }}
+          exit={{ y: -12 }}
+          className="text-red-500"
+        >
+          {errorProp}
+        </motion.div>
       </motion.div>
     </AnimatePresence>
   );
