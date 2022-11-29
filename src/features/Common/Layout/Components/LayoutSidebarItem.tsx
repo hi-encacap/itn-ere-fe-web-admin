82c904a695e064a274037d4b783e4c3f3a59a651
Khanh Nguyen <encacap@gmail.com>
2022-11-29 18:07:00 +0700

    fix: update sidebar

--- src/features/Common/Layout/Components/LayoutSidebarItem.tsx
+++ src/features/Common/Layout/Components/LayoutSidebarItem.tsx
@@ -14,11 +14,11 @@ const LayoutSidebarItem = ({ icon, label, to, isActive = false }: LayoutSidebarI
     <Link
       to={to}
       className={twMerge(
-        'relative cursor-pointer rounded-md px-5 py-2 duration-100 hover:bg-gray-100',
+        'relative cursor-pointer rounded-md px-5 py-2.5 duration-100 hover:bg-gray-100',
         isActive && 'bg-gray-100',
       )}
     >
-      {isActive && <div className="absolute left-0 top-2.5 h-5 w-1 rounded-r-sm bg-teal-400" />}
+      {isActive && <div className="absolute left-0 top-3 h-5 w-1 rounded-r-sm bg-teal-400" />}
       <div className="flex items-center justify-start space-x-4">
         {cloneElement(icon, {
           className: twMerge('w-5 h-5 block flex-shrink-0', isActive && 'text-teal-400'),
