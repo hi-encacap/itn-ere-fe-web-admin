04f3b8660b76fcbf6e6f4effb00c51709adf9c4c
Khanh Nguyen <encacap@gmail.com>
2022-12-01 21:34:28 +0700

    feat: modal and confirmation modal components

---
+++ src/features/Common/Components/Modal/ConfirmationModal.tsx
@@ -0,0 +1,118 @@
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { FiAlertTriangle } from 'react-icons/fi';
+
+import { ConfirmationModalStatusType } from '@interfaces/Common/elementTypes';
+
+import { Button } from '@components/Form';
+
+import Modal, { ModalProps } from './Modal';
+
+export interface ConfirmationModalProps extends ModalProps {
+  title: string;
+  message: string;
+  status?: ConfirmationModalStatusType;
+  cancelButtonText?: string;
+  confirmButtonText?: string;
+  onConfirm: () => void;
+}
+
+const ConfirmationModal = ({
+  isOpen,
+  status = 'success',
+  title,
+  message,
+  cancelButtonText,
+  confirmButtonText,
+  onConfirm,
+  onClose,
+  ...props
+}: ConfirmationModalProps) => {
+  const { t } = useTranslation(['common'], {
+    keyPrefix: 'common:confirmationModal',
+  });
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleClickConfirmButton = () => {
+    setIsSubmitting(true);
+    onConfirm();
+  };
+
+  const handleClickCancelButton = () => {
+    onClose();
+  };
+
+  const getClassNameByStatus = (statusParam: ConfirmationModalStatusType) => {
+    if (statusParam === 'success') {
+      return 'flex items-center justify-center flex-shrink-0 w-12 h-12 mx-auto bg-green-100 rounded-full sm:mx-0 sm:h-10 sm:w-10';
+    }
+    if (statusParam === 'danger') {
+      return 'flex items-center justify-center flex-shrink-0 w-12 h-12 mx-auto bg-red-100 rounded-full sm:mx-0 sm:h-10 sm:w-10';
+    }
+    return 'flex items-center justify-center flex-shrink-0 w-12 h-12 mx-auto bg-red-100 rounded-full sm:mx-0 sm:h-10 sm:w-10';
+  };
+
+  const getIconClassNameByStatus = (statusParam: ConfirmationModalStatusType) => {
+    if (statusParam === 'success') {
+      return 'text-green-500';
+    }
+    if (statusParam === 'danger') {
+      return 'text-red-500';
+    }
+    return 'text-red-500';
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      setIsSubmitting(false);
+    }
+  }, [isOpen]);
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      isShowHeader={false}
+      isShowFooter={false}
+      contentContainerClassName="max-w-md"
+      onClose={onClose}
+      {...props}
+    >
+      <div className="-mx-10 -my-2 pl-6 sm:flex sm:items-start">
+        <div className={getClassNameByStatus(status)}>
+          <FiAlertTriangle size={18} className={getIconClassNameByStatus(status)} />
+        </div>
+        <div className="text-center sm:mx-6 sm:mt-0 sm:text-left">
+          <h3 className="mt-2 text-lg font-medium leading-6 text-gray-900">{title}</h3>
+          <div className="mt-4">
+            <p className="text-sm text-gray-500">{message}</p>
+          </div>
+        </div>
+      </div>
+      <div className="-mx-10 mt-9 -mb-8 flex items-center justify-end space-x-4 rounded-b-lg bg-gray-50 px-6 py-5">
+        <Button
+          type="button"
+          size="sm"
+          color="light"
+          className="rounded-md border-2 border-gray-200 shadow-none ring-0"
+          onClick={handleClickCancelButton}
+          disabled={isSubmitting}
+        >
+          {cancelButtonText ?? t('cancelButtonText')}
+        </Button>
+        <Button
+          type="button"
+          size="sm"
+          className="border-primary-700 rounded-md border-2 px-12 shadow-none ring-0 disabled:border-gray-300"
+          disabled={isSubmitting}
+          isLoading={isSubmitting}
+          onClick={handleClickConfirmButton}
+        >
+          {confirmButtonText ?? t('confirmButtonText')}
+        </Button>
+      </div>
+    </Modal>
+  );
+};
+
+export default ConfirmationModal;
---
+++ src/features/Common/Components/Modal/Modal.tsx
@@ -0,0 +1,135 @@
+import { Dialog } from '@headlessui/react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { forwardRef } from 'react';
+import { useTranslation } from 'react-i18next';
+import { twMerge } from 'tailwind-merge';
+
+import { Button } from '@components/Form';
+
+export interface ModalProps extends React.HTMLAttributes<HTMLDivElement> {
+  isOpen: boolean;
+  isShowHeader?: boolean;
+  isShowFooter?: boolean;
+  isAllowSubmit?: boolean;
+  isLoading?: boolean;
+  contentContainerClassName?: string;
+  title?: string;
+  onClose: () => void;
+  onConfirm?: () => void;
+}
+
+const Modal = (
+  {
+    isOpen,
+    isShowHeader = true,
+    isShowFooter = true,
+    isAllowSubmit = true,
+    isLoading = false,
+    title,
+    children,
+    className,
+    contentContainerClassName,
+    onClose,
+    onConfirm,
+  }: ModalProps,
+  ref: React.Ref<HTMLDivElement>,
+) => {
+  const { t } = useTranslation(['common'], {
+    keyPrefix: 'common:modal',
+  });
+
+  const modalVariants = {
+    hidden: {
+      transform: 'scale(0.95)',
+      opacity: 0,
+      transition: {
+        delay: 0.1,
+      },
+    },
+    visible: {
+      transform: 'scale(1)',
+      opacity: 1,
+      transition: {
+        duration: 0.2,
+      },
+    },
+    exit: {
+      transform: 'scale(0.95)',
+      opacity: 0,
+      transition: {
+        duration: 0.2,
+      },
+    },
+  };
+
+  return (
+    <AnimatePresence initial={false} mode="wait" onExitComplete={() => null}>
+      {isOpen && (
+        <Dialog
+          ref={ref}
+          open={isOpen}
+          as="div"
+          className={twMerge(className, 'fixed inset-0 z-50 flex justify-center overflow-y-auto')}
+          onClose={onClose}
+        >
+          <div className="my-auto flex max-h-full">
+            <motion.div
+              initial={{ opacity: 0, scale: 0.95 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 0,
+                delay: 0,
+                ease: 'easeIn',
+                times: [0, 0.71, 0.2, 1.01],
+              }}
+            >
+              <Dialog.Overlay className="fixed inset-0 z-0 bg-black bg-opacity-75 transition-opacity" />
+            </motion.div>
+
+            <motion.div variants={modalVariants} initial="hidden" animate="visible" exit="exit">
+              <div className="relative z-20 w-full md:m-auto md:rounded-lg">
+                <div className={twMerge('h-fit w-fit rounded-lg bg-white', isShowHeader && 'pt-7')}>
+                  <div className="px-10">
+                    {isShowHeader && (
+                      <div>
+                        <div className="h-fit w-fit pb-2 text-lg font-semibold">{title}</div>
+                        <div className="mt-2 h-1 w-16 rounded-md bg-gray-100" />
+                      </div>
+                    )}
+                    <div className={twMerge('w-[400px] pt-8 pb-8', contentContainerClassName)}>
+                      {children}
+                    </div>
+                  </div>
+                  {isShowFooter && (
+                    <div className="flex items-center justify-end space-x-6 rounded-b-lg bg-gray-50 px-10 py-6">
+                      <Button
+                        className="rounded-md border-2 border-gray-200 shadow-none ring-0"
+                        size="sm"
+                        color="light"
+                        disabled={isLoading}
+                        onClick={onClose}
+                      >
+                        {t('close')}
+                      </Button>
+                      <Button
+                        className="border-primary-700 rounded-md border-2 px-12 shadow-none ring-0 disabled:border-gray-300"
+                        size="sm"
+                        disabled={isLoading || !isAllowSubmit}
+                        isLoading={isLoading}
+                        onClick={onConfirm}
+                      >
+                        {t('confirm')}
+                      </Button>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </motion.div>
+          </div>
+        </Dialog>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default forwardRef(Modal);
