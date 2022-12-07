9bee84659e82b3c49ee277282892c740c62379f2
Khanh Nguyen <encacap@gmail.com>
2022-12-07 22:19:01 +0700

    fix: move table image column to common

--- src/features/Admin/Contact/Components/AdminContactTableImageColumnSkeleton.tsx
+++
@@ -1,7 +0,0 @@
-import { LoadingSkeleton } from '@components/Loading';
-
-const AdminContactTableImageColumnSkeleton = () => {
-  return <LoadingSkeleton className="mx-auto h-12 w-12 rounded-full" />;
-};
-
-export default AdminContactTableImageColumnSkeleton;
