eb67833572cd4fed48bce85289465d1a24f51278
Nguyễn Khắc Khánh <encacap@gmail.com>
2022-12-15 21:37:47 +0700

    fix: set max height of select component based on position (#31)
    * fix: increase header z-index
    
    * fix: set max height of select component based on position
    
    * fix: resolve variable declared issue

--- src/features/Common/Components/Form/Select/SelecOptionDropdown.tsx
+++ src/features/Common/Components/Form/Select/SelecOptionDropdown.tsx
@@ -1,3 +1,5 @@
+import { RefObject, useMemo } from 'react';
+
 import { SelectOptionItemType } from '@interfaces/Common/elementTypes';
 
 import SelectOptionDropdownItem from './SelectOptionDropdownItem';
@@ -7,19 +9,41 @@ type OptionValueType = SelectOptionItemType['value'];
 export interface SelectOptionDropdownProps {
   options: SelectOptionItemType[];
   value?: OptionValueType;
+  inputRef?: RefObject<HTMLInputElement>;
   onFocus?: () => void;
   onBlur?: () => void;
   onChange: (value: OptionValueType) => void;
 }
 
-const SelectOptionDropdown = ({ value, options, onFocus, onBlur, onChange }: SelectOptionDropdownProps) => {
+const SelectOptionDropdown = ({
+  value,
+  options,
+  inputRef,
+  onFocus,
+  onBlur,
+  onChange,
+}: SelectOptionDropdownProps) => {
   if (!options.length) {
     return null;
   }
 
+  const dropdownHeight = useMemo(() => {
+    const inputElement = inputRef?.current;
+
+    if (!inputElement) {
+      return 0;
+    }
+
+    const inputPosition = inputElement.getBoundingClientRect();
+    const screenPosition = window.screen.availHeight;
+
+    return screenPosition - inputPosition.bottom - 120;
+  }, [inputRef]);
+
   return (
     <div
-      className="absolute -left-0.5 -right-0.5 top-14 z-10 rounded-md bg-white py-2 shadow-center"
+      className="overflow-overlay absolute -left-0.5 -right-0.5 top-14 z-10 rounded-md bg-white py-2 shadow-center"
+      style={{ maxHeight: dropdownHeight }}
       onMouseEnter={onFocus}
       onMouseLeave={onBlur}
     >
--- src/features/Common/Layout/Components/LayoutHeader.tsx
+++ src/features/Common/Layout/Components/LayoutHeader.tsx
@@ -2,7 +2,7 @@ import { Logo } from '@components/Logo';
 
 const LayoutHeader = () => {
   return (
-    <div className="fixed top-0 left-0 right-0 flex h-20 items-center justify-between border-b-2 border-gray-100 bg-white px-8">
+    <div className="fixed top-0 left-0 right-0 z-20 flex h-20 items-center justify-between border-b-2 border-gray-100 bg-white px-8">
       <Logo className="w-11" />
     </div>
   );
