b2913f4222f93d8c9955e0926553bfb5899bc385
Khanh Nguyen <encacap@gmail.com>
2022-12-26 21:18:23 +0700

    feat(ERE-29): estate management sidebar

--- src/features/Common/Layout/Components/LayoutSidebarItemChildrenItem.tsx
+++ src/features/Common/Layout/Components/LayoutSidebarItemChildrenItem.tsx
@@ -17,14 +17,14 @@ const LayoutSidebarItemChildrenItem = ({ item }: LayoutSidebarItemChildrenItemPr
     <Link
       to={item.to}
       className={twMerge(
-        'flex items-center justify-start space-x-4 rounded-md px-9 py-2 hover:bg-gray-100',
+        'flex items-center justify-start space-x-4 rounded-md py-2 pl-9 pr-4 hover:bg-gray-100',
         isActive && 'bg-gray-100',
       )}
     >
       {cloneElement(item.icon, {
         className: 'flex-shrink-0 w-4.5 h-4.5',
       })}
-      <div className="">{item.label}</div>
+      <div className="line-clamp-1">{item.label}</div>
     </Link>
   );
 };
