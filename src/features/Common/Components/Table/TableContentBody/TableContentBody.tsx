1f711db09fe1042c8b9f072e08e8b6425aad3839
Khanh Nguyen <encacap@gmail.com>
2023-04-25 21:05:58 +0700

    fix: resolve deepsource issues

--- src/features/Common/Components/Table/TableContentBody/TableContentBody.tsx
+++ src/features/Common/Components/Table/TableContentBody/TableContentBody.tsx
@@ -19,6 +19,7 @@ const TableContentBody = ({ rows, headers, isLoading = false }: TableContentProp
       {rows.length === 0 &&
         isLoading &&
         Array.from({ length: totalSkeletonItems }).map((_, index) => (
+          // #skip-cq: JS-0437
           // eslint-disable-next-line react/no-array-index-key
           <TableContentBodySkeleton key={index} headers={headers} />
         ))}
