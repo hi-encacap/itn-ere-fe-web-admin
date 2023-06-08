b4283f958f44eb9f42d0deb4dc13d5fecc741d6f
Khanh Nguyen <encacap@gmail.com>
2023-06-08 21:15:18 +0700

    feat: show notification when accessing by mobile

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,10 @@ const App = () => {
       </BrowserRouter>
       <DropdownProvider />
       <ToastContainer />
+      <div className="fixed inset-0 z-70 flex items-center justify-center bg-white px-20 text-center lg:hidden">
+        Trang này hiện không thể sử dụng trên thiết bị di động. Vui lòng truy cập trên một màn hình có kích
+        thước lớn hơn.
+      </div>
     </Provider>
   );
 };
