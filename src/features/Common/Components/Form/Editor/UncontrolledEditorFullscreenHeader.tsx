2b88f71d2de46e5acdbac80fd03b47ba7a1caf36
Khanh Nguyen <encacap@gmail.com>
2023-06-09 10:53:03 +0700

    feat: automatically enter full screen the editor when user editing content

---
+++ src/features/Common/Components/Form/Editor/UncontrolledEditorFullscreenHeader.tsx
@@ -0,0 +1,32 @@
+import { createPortal } from "react-dom";
+import { useTranslation } from "react-i18next";
+import { BsArrowLeft } from "react-icons/bs";
+
+import Button from "../Button/Button";
+
+interface UncontrolledEditorFullscreenHeaderProps {
+  title?: string | null;
+  onClose: () => void;
+}
+
+const UncontrolledEditorFullscreenHeader = ({ title, onClose }: UncontrolledEditorFullscreenHeaderProps) => {
+  const { t } = useTranslation();
+
+  return createPortal(
+    <div className="fixed inset-x-0 top-0 z-70 flex h-20 items-center justify-between border-b-2 border-gray-100 bg-white px-5">
+      <div className="flex items-center justify-center space-x-5">
+        <button
+          className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-full border-2 border-gray-100 duration-100 hover:bg-gray-50"
+          onClick={onClose}
+        >
+          <BsArrowLeft />
+        </button>
+        <span>{title}</span>
+      </div>
+      <Button onClick={onClose}>{t("confirm")}</Button>
+    </div>,
+    window.document.querySelector("#root") as HTMLElement,
+  );
+};
+
+export default UncontrolledEditorFullscreenHeader;
